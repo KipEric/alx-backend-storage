1c5c71b35dc1bc582b98f3ada7147f5aa4348109
KipEric <kipkoecheric97@gmail.com>
2023-07-20 19:29:28 +0300

    Task 0 and 1 c

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Cache class for storing data
 """
@@ -14,15 +14,12 @@ class Cache:
     """
     Cache class for storing data in Redis
     """
-
-
     def __init__(self):
         """
         Initializing the cache instance
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
-
 
     @staticmethod
     def count_calls(method: Callable) -> Callable:
@@ -36,7 +33,6 @@ class Cache:
             return method(self, *args, **kwargs)
         return wrapped
 
-
     @staticmethod
     def call_history(method: Callable) -> Callable:
         """
@@ -46,22 +42,27 @@ class Cache:
         def wrapped(self, *args, **kwargs):
             input_key = "{}:inputs".format(method.__qualname__)
             output_key = "{}:output".format(method.__qualname__)
-
-
             self.__redis.rpush(input_key, str(args))
-
-
             result = method(self, *args, **kwargs)
-
-
             self.__redis.rpush(output_key, result)
-
-
             return result
         return wrapped
 
+    @staticmethod
+    def get_str(data: Union[str, bytes, int, float]) -> str:
+        """
+        Method to convert data to string
+        """
+        return str(data)
 
-    def store(self, data: Union[str,bytes, int, float]) -> str:
+    @staticmethod
+    def get_int(data: Union[str, bytes, int, float]) -> int:
+        """
+        Method to convert data to integer
+        """
+        return int(data)
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store inpute data in Redis
         """
@@ -69,8 +70,8 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Callable = None) -> Union[
+            str, bytes, int, float, None]:
         """
         Retriving data from Redis
         """
@@ -79,19 +80,16 @@ class Cache:
             return fn(data)
         return data
 
-
     def replay(method: Callable):
         """
         Displays the history of calls
         """
         input_key = "{}:inputs".format(method.__qualname__)
         output_key = "{}:output".format(method.__qualname__)
-
-
         inputs = method._redis.lrange(input_key, 0, -1)
         outputs = method._redis.lrange(output_key, 0, -1)
-
-
-        print("{} was called {} times".format(method.__qualname__, len(inputs)))
+        print("{} was called {} times".format(
+            method.__qualname__, len(inputs)))
         for args, output in zip(inputs, outputs):
-            print("{}{} -> {}".format(method.__qualname__, args, output))
+            print("{}{} -> {}".format(
+                method.__qualname__, args, output))
