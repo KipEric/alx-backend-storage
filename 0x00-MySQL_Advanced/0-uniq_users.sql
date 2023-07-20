f8b41a0754bdd9db829a0bbcb52e2fdf43846fa7
KipEric <kipkoecheric97@gmail.com>
2023-07-20 07:39:37 +0300

    Task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,7 @@
+--We are all unique!
+
+
+--If table exist ignore the error and continue
 CREATE TABLE IF NOT EXIST users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
